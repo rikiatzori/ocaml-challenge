0fe7dd3c83efe6ab5591982765d07e6a99d9e7a2
rikiatzori <rikiatzori99@gmail.com>
2024-10-16 22:38:50 +0200

    esercizio valore assoluto

---
+++ 1/7-11-doubles/Val_Assoluto.ml
@@ -0,0 +1,13 @@
+(* Simulazione VALORE ASSOLUTO *)
+
+let valore_Assoluto r= if (r < 0) then -(r) else r ;;
+let valore_assoluto r =
+  match r < 0 with
+  | true -> -r
+  | false -> r ;;
+
+
+
+
+
+
